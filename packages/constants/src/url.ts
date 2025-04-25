5ff3c0cb48c5322119cf5888c8e522b883ba69d8
Ribbit <echo07200503@gmail.com>
2025-04-26 06:48:31 +0900

    📝 jsdocを追加

--- packages/constants/src/url.ts
+++ packages/constants/src/url.ts
@@ -1,3 +1,14 @@
-export const URL_INQUIRY = 'https://form.konomi.app';
+/**
+ * プラグインでエラーが発生した際などに表示する、お問い合わせ先のURL
+ */
+export const URL_INQUIRY = 'https://konomi.app/contact';
+
+/**
+ * プラグイン設定画面のフッター部分に埋め込むページのURL
+ */
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
+
+/**
+ * プラグインの右側に埋め込むバナーのURL
+ */
 export const URL_BANNER = 'https://promotion.konomi.app/kintone-plugin/sidebar';
