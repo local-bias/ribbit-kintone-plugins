62b4fe05654bb4f83e0e7a1d1c13452fc3dd8d27
Ribbit <echo07200503@gmail.com>
2025-04-27 18:48:13 +0900

    ✨ i18n を使用してプラグイン設定のルート要素のエラーメッセージを国際化

--- apps/editable/src/config/main.tsx
+++ apps/editable/src/config/main.tsx
@@ -1,7 +1,8 @@
+import { t } from '@/lib/i18n';
 import { createRoot } from 'react-dom/client';
 import invariant from 'tiny-invariant';
 import App from './app';
 
 const root = document.getElementById('settings');
-invariant(root, 'プラグイン設定のルート要素が見つかりません。');
+invariant(root, t('common.config.error.rootNotFound'));
 createRoot(root).render(<App />);
