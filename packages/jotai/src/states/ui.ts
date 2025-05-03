bdac1907eac958ffaa31d11273766eff5d89b8c4
Ribbit <echo07200503@gmail.com>
2025-05-03 15:51:15 +0900

    ✨ ローディング状態を管理するatomのコメントを改善し、使用例を追加

--- packages/jotai/src/states/ui.ts
+++ packages/jotai/src/states/ui.ts
@@ -1,28 +1,67 @@
 import { atom } from 'jotai';
 
 /**
- * ローディングカウンターを管理する内部atom
- * 複数の並行処理のローディング状態を追跡する
+ * 現在実行中の処理の数を管理するatom
+ * このatomを直接変更することはなく、変更はhandleLoadingStartAtomとhandleLoadingEndAtomを使用し
+ * いずれかの処理が実行中であることはloadingAtomを使用して取得する
+ *
+ * @see {@link loadingAtom}
+ * @see {@link handleLoadingStartAtom}
+ * @see {@link handleLoadingEndAtom}
  */
 const loadingCountAtom = atom(0);
 
 /**
- * 何らかの処理が実行中かどうかを示すatom
+ * ⏳ 何らかの処理が実行中かどうかを示すatom
+ *
+ * このatomは読み取り専用で、直接変更することはできません
+ *
+ * 読み込み中の状態を変更するには、{@link handleLoadingStartAtom}または{@link handleLoadingEndAtom}を使用してください
  */
 export const loadingAtom = atom((get) => get(loadingCountAtom) > 0);
 
 /**
- * ローディング状態を開始するためのatom
- * このatomに対してwrite操作を行うとローディングカウンターが増加する
+ * ⏳ 処理が開始されたことを示すatom
+ *
+ * このatomは、処理が開始されたときに呼び出され、loadingCountAtomをインクリメントします
+ *
+ * {@link loadingAtom}を使用して、現在の読み込み状態を確認できます
+ *
+ * @example
+ * ```ts
+ * // 想定される使用例 (atomCallback もしくは write only atom の中で)
+ * try {
+ *   set(handleLoadingStartAtom);
+ *
+ *   // ここで何らかの処理を実行
+ * } finally {
+ *   set(handleLoadingEndAtom);
+ * }
+ * ```
  */
-export const loadingStartAtom = atom(null, (_, set) => {
+export const handleLoadingStartAtom = atom(null, (_, set) => {
   set(loadingCountAtom, (count) => count + 1);
 });
 
 /**
- * ローディング状態を終了するためのatom
- * このatomに対してwrite操作を行うとローディングカウンターが減少する
+ * ⏳ 処理が終了したことを示すatom
+ *
+ * このatomは、処理が終了したときに呼び出され、loadingCountAtomをデクリメントします
+ *
+ * {@link loadingAtom}を使用して、現在の読み込み状態を確認できます
+ *
+ * @example
+ * ```ts
+ * // 想定される使用例 (atomCallback もしくは write only atom の中で)
+ * try {
+ *   set(handleLoadingStartAtom);
+ *
+ *   // ここで何らかの処理を実行
+ * } finally {
+ *   set(handleLoadingEndAtom);
+ * }
+ * ```
  */
-export const loadingEndAtom = atom(null, (_, set) => {
+export const handleLoadingEndAtom = atom(null, (_, set) => {
   set(loadingCountAtom, (count) => Math.max(count - 1, 0));
 });
