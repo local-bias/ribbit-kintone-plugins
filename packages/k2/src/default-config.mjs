2116f2c61708c5df5b0bc0be38a6191bd81bdeec
Ribbit <echo07200503@gmail.com>
2025-05-11 12:42:49 +0900

    ✨ k2パッケージの初期設定ファイルを追加: package.json, default-config.mjs, tsconfig.jsonを作成

---
+++ packages/k2/src/default-config.mjs
@@ -0,0 +1,57 @@
+// @ts-check
+const hp = 'https://konomi.app';
+const cdn = 'https://kintone-plugin.konomi.app';
+// const pluginReleasePageUrl = 'https://ribbit.konomi.app/kintone-plugin/';
+
+/** @satisfies { Partial<Plugin.Meta.Config["manifest"]["base"]> } */
+export const manifestBase = /** @type { const } */ ({
+  manifest_version: 1,
+  type: 'APP',
+  icon: 'icon.png',
+  homepage_url: { ja: hp, en: hp, zh: hp, 'zh-TW': hp, es: hp, 'pt-BR': hp, th: hp },
+  desktop: { js: [`${cdn}/common/desktop.js`], css: [`${cdn}/common/desktop.css`] },
+  mobile: { js: [`${cdn}/common/desktop.js`], css: [`${cdn}/common/desktop.css`] },
+  config: {
+    html: 'config.html',
+    js: [`${cdn}/common/config.js`],
+    css: [`${cdn}/common/config.css`],
+    required_params: [],
+  },
+});
+
+// /** @satisfies { Partial<Plugin.Meta.Config> } */
+// export default /** @type { const } */ ({
+//   pluginReleasePageUrl,
+//   server: {
+//     port: 5500,
+//   },
+//   lint: {
+//     build: false,
+//   },
+//   manifest: {
+//     base: {
+//       manifest_version: 1,
+//       type: 'APP',
+//       icon: 'icon.png',
+//       homepage_url: { ja: hp, en: hp, zh: hp, 'zh-TW': hp, es: hp, 'pt-BR': hp, th: hp },
+//       desktop: { js: [`${cdn}/common/desktop.js`], css: [`${cdn}/common/desktop.css`] },
+//       mobile: { js: [`${cdn}/common/desktop.js`], css: [`${cdn}/common/desktop.css`] },
+//       config: {
+//         html: 'config.html',
+//         js: [`${cdn}/common/config.js`],
+//         css: [`${cdn}/common/config.css`],
+//         required_params: [],
+//       },
+//     },
+//     prod: {
+//       desktop: { js: [`${cdn}/${key}/desktop.js`], css: [`${cdn}/${key}/desktop.css`] },
+//       mobile: { js: [`${cdn}/${key}/desktop.js`], css: [`${cdn}/${key}/desktop.css`] },
+//       config: { js: [`${cdn}/${key}/config.js`], css: [`${cdn}/${key}/config.css`] },
+//     },
+//     standalone: {
+//       desktop: { js: ['desktop.js'], css: ['desktop.css'] },
+//       mobile: { js: ['desktop.js'], css: ['desktop.css'] },
+//       config: { js: ['config.js'], css: ['config.css'] },
+//     },
+//   },
+// });
