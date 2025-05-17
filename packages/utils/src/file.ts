3b14e2dbee5dc1111fcbfe4843d6d4925284fe17
adachi <echo07200503@gmail.com>
2025-05-17 10:08:41 +0900

    ✨ JSONデータをファイルとして保存する関数を追加

--- packages/utils/src/file.ts
+++ packages/utils/src/file.ts
@@ -1,1 +1,8 @@
-// file
+import { saveAs } from 'file-saver';
+
+export function saveAsJson(data: unknown, filename: string) {
+  const blob = new Blob([JSON.stringify(data, null)], {
+    type: 'application/json',
+  });
+  saveAs(blob, filename);
+}
