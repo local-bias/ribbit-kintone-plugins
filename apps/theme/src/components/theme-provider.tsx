ee8271da4f52633aa883f5fc3aef7829f37b7dd2
adachi <echo07200503@gmail.com>
2025-05-24 10:22:05 +0900

    ♻ ThemeProviderコンポーネントの定義をFCから通常の関数に変更

--- apps/theme/src/components/theme-provider.tsx
+++ apps/theme/src/components/theme-provider.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider as MUIThemeProvider } from '@mui/material';
-import { FC, PropsWithChildren } from 'react';
 import { getMUITheme } from '../lib/i18n-mui';
 
-export const ThemeProvider: FC<PropsWithChildren> = ({ children }) => (
-  <MUIThemeProvider theme={getMUITheme()}>{children}</MUIThemeProvider>
-);
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return <MUIThemeProvider theme={getMUITheme()}>{children}</MUIThemeProvider>;
+}
