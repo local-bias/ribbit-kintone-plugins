87bbbbbc102aef13c62dfc468d4209fe1b5c1f75
adachi <echo07200503@gmail.com>
2025-06-01 00:19:05 +0900

    🔖 バージョン番号を1.0.0から1.0.1に更新

--- apps/pdf-preview/plugin.config.mjs
+++ apps/pdf-preview/plugin.config.mjs
@@ -16,7 +16,7 @@ export default /** @type { const } */ ({
   manifest: {
     base: {
       manifest_version: 1,
-      version: '1.0.0',
+      version: '1.0.1',
       type: 'APP',
       name: {
         en: 'PDF Preview Plugin',
