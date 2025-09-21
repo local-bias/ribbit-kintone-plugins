c2e1a4fa6e245c08f7ca45d10c3e91ead2318813
adachi <echo07200503@gmail.com>
2025-09-21 13:15:51 +0900

    ♻  invariantを使用してルート要素の存在を検証するように修正

--- apps/autocomplete/src/config/main.tsx
+++ apps/autocomplete/src/config/main.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
+import invariant from 'tiny-invariant';
 import App from './app';
 
 const root = document.getElementById('settings');
-if (!root) {
-  throw new Error(
-    'プラグインのHTMLに、ルート要素が存在しません。プラグイン設定をレンダリングするためには、id="settings"の要素が必要です。'
-  );
-}
+invariant(root, 'プラグインのHTMLに、ルート要素が存在しません。プラグイン設定をレンダリングするためには、id="settings"の要素が必要です。');
 createRoot(root).render(<App />);
