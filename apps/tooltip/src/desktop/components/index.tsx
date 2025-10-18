51d5d9ebecadf7b685c08a17420505dd2a8492aa
adachi <echo07200503@gmail.com>
2025-10-19 08:06:38 +0900

    🔧 インポートを整理し、TooltipContentのスタイルをCSS-in-JSに変更

--- apps/tooltip/src/desktop/components/index.tsx
+++ apps/tooltip/src/desktop/components/index.tsx
@@ -1,18 +1,26 @@
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@repo/ui';
 import { PluginCondition } from '@/schema/plugin-config';
 import { isMobile } from '@konomi-app/kintone-utilities';
 import DOMPurify from 'dompurify';
 import { type FC } from 'react';
 import TooltipEmojiContainer from './emoji';
 import TooltipIconContainer from './icon';
+import { css } from '@emotion/css';
 
 type Props = {
   condition: PluginCondition;
 };
 
 const commonCss =
-  'rad:absolute rad:left-0 rad:p-0 rad:top-1/2 rad:-translate-y-1/2 rad:grid rad:place-items-center';
+  'rad:absolute rad:left-0 rad:p-0 rad:top-1/2 rad:-translate-y-1/2 rad:grid rad:place-items-center rad:border-0 rad:bg-transparent rad:shadow-none rad:cursor-pointer';
 
 const MobileTooltipContainer: FC<Props> = ({ condition }) => {
   return (
@@ -45,10 +53,15 @@ const DesktopTooltipContainer: FC<Props> = ({ condition }) => {
             <TooltipEmojiContainer condition={condition} />
           </TooltipTrigger>
           <TooltipContent
-            style={{
-              backgroundColor: condition.backgroundColor,
-              color: condition.foregroundColor,
-            }}
+            className={css`
+              background-color: ${condition.backgroundColor};
+              color: ${condition.foregroundColor};
+
+              svg {
+                fill: ${condition.backgroundColor};
+                background-color: ${condition.backgroundColor};
+              }
+            `}
           >
             <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(condition.label) }} />
           </TooltipContent>
