7f577b9759206e7c20c97b4649959c56c6fdb0cd
adachi <echo07200503@gmail.com>
2025-12-21 07:16:07 +0900

    ♻ ThemeProviderコンポーネントの定義を関数形式に変更

--- apps/autocomplete/src/lib/components/theme-provider.tsx
+++ apps/autocomplete/src/lib/components/theme-provider.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider as MUIThemeProvider } from '@mui/material';
-import React, { FC, PropsWithChildren } from 'react';
 import { getMUITheme } from '../i18n';
 
-export const ThemeProvider: FC<PropsWithChildren> = ({ children }) => (
-  <MUIThemeProvider theme={getMUITheme()}>{children}</MUIThemeProvider>
-);
+export function ThemeProvider({ children }: { children: React.ReactNode; }) {
+  return <MUIThemeProvider theme={getMUITheme()}>{children}</MUIThemeProvider>;
+}
