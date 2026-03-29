6dd68127698ac2f3c661ed75f412c6192256ccef
adachi <adachi@konomi.app>
2026-03-29 15:52:48 +0900

    ✨ ルート要素の存在確認をinvariantに置き換え

--- apps/xlsx/src/config/main.tsx
+++ apps/xlsx/src/config/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import invariant from 'tiny-invariant';
 import App from './app';
 
 const root = document.getElementById('settings');
-if (!root) {
-  throw 'プラグインのHTMLに、ルート要素が存在しません。';
-}
+invariant(root, 'プラグインのHTMLに、ルート要素が存在しません。');
 createRoot(root).render(<App />);
