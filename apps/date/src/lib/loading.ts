8a3233c78d0ecace32ec1723679928369b3c60e3
adachi <adachi@konomi.app>
2026-08-01 08:10:57 +0900

    🔥 未使用の関数を削除

--- apps/date/src/lib/loading.ts
+++
@@ -1,5 +0,0 @@
-import { LoadingOverlay } from '@konomi-app/ui';
-
-const singleton = new LoadingOverlay();
-
-export const loadingOverlay = singleton;
--- apps/image-converter/src/lib/ui.ts
+++
@@ -1,6 +0,0 @@
-import { LoadingOverlay } from '@konomi-app/ui';
-
-const singleton = new LoadingOverlay();
-
-/** ロード画面操作用クラスのシングルトン */
-export const loadingOverlay = singleton;
